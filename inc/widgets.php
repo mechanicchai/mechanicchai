d83b861d12d1098e2e7e81aa6cf9bbb189983210
Nazrul Islam Nayan <43903460+nayanchamp7@users.noreply.github.com>
2020-04-13 00:23:17 +0600

    added column three

--- inc/widgets.php
+++ inc/widgets.php
@@ -19,7 +19,7 @@ if ( ! function_exists( 'mc_widgets_initialize' ) ) {
 			'name'          => __( 'Footer widget', 'everstrap' ),
 			'id'            => 'footer-widget',
 			'description'   => 'You may set maximum 3 widget here.',
-			'before_widget' => '<div class="col-md-4 col-sm-4"><div id="%1$s" class="footer-widget footer-menu-widget %2$s">',
+			'before_widget' => '<div class="col-lg-3 col-md-6"><div id="%1$s" class="footer-widget footer-menu-widget %2$s">',
 			'after_widget'  => '</div></div>',
 			'before_title'  => '<h3 class="footer-widget-title">',
 			'after_title'   => '</h3>',
