d69254555f5a027bb4d2c6dc2a5c5b27fecb2c54
Nazrul Islam Nayan <43903460+nayanchamp7@users.noreply.github.com>
2020-07-19 01:20:20 +0600

    resolve only 5query problem

--- page-templates/service.php
+++ page-templates/service.php
@@ -198,6 +198,7 @@ get_header();
                                         <?php
                                         $args = array(
                                             'post_type' => 'service',
+                                            'posts_per_page' => -1,
                                             'meta_query' => array(
                                                 array(
                                                     'key' => 'mc_service_type',
@@ -256,7 +257,7 @@ get_header();
                                 <?php
                                 $args = array(
                                     'post_type' => 'service',
-                                    'posts_per_page' => 30,
+                                    'posts_per_page' => -1,
                                     'meta_query' => array(
                                         array(
                                             'key' => 'mc_service_type',
@@ -266,10 +267,6 @@ get_header();
                                     )
                                 );
                                 $diagnosis_service_posts = get_posts($args);
-
-                                echo '<pre>';
-                                print_r($diagnosis_service_posts);
-                                echo '</pre>';
 
                                 if ($diagnosis_service_posts) {
                                     foreach ($diagnosis_service_posts as $diagnosis_service_post) {
